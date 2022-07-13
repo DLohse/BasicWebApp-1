8b852a6c60a2a0ff6d9c83e6b85a9bd5664c43bb
David Lohse <david.lohse@tum.de>
2022-07-13 15:29:29 +0200

    addition

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,11 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "david";
-        } else { // TODO extend the programm here
+        } else if (query.toLowerCase().contains("plus")) {
+            String[] splitquery = query.split(" ");
+            return String.valueOf(Integer.parseInt(splitquery[2]) + Integer.parseInt(splitquery[4]));
+        }
+        else { // TODO extend the programm here
             return "";
         }
     }
